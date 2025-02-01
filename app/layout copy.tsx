65449e1a484f13cf998e9ebb188c158582feddf1
mervesekerfilizoglu <sekermer33@gmail.com>
2025-02-01 18:07:10 +0300

    change back tick

--- app/layout copy.tsx
+++ app/layout copy.tsx
@@ -1,37 +1,28 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { Inter, IBM_Plex_Serif } from "next/font/google";
 import "./globals.css";
+import { Inter, IBM_Plex_Serif } from 'next/font/google';
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+
+const ibmPlexSerif = IBM_Plex_Serif({ subsets: ["latin"], weight: ["400", "700"], variable: "--font-ibm-plex-serif" });
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
-
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
-const inter = Inter({
-  variable: "--font-inter",
-  subsets: ["latin"],
-});
-
-const ibmPlexSerif = IBM_Plex_Serif({
-  variable: "--font-ibm-plex-serif",
-  weight:["400", "700"],
-  subsets: ["latin"],
-});
-
 export const metadata: Metadata = {
   title: "Horizon",
   description: "Horizon is a modern banking platform for everyone",
-  icons: 
+  icons:
   {
-    icon: `/icons/logo.svg`
+    icon:`/icons/logo.svg`
   }
 };
-
 export default function RootLayout({
   children,
 }: Readonly<{
