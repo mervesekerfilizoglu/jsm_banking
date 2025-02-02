9b3cbebc5756ad5cf3dda3d0ee5790212a64a8e6
mervesekerfilizoglu <sekermer33@gmail.com>
2025-02-02 17:57:37 +0300

    layout edited.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,37 +1,25 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { Inter, IBM_Plex_Serif } from "next/font/google";
 import "./globals.css";
-
+import { Inter, IBM_Plex_Serif } from 'next/font/google';
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+const ibmPlexSerif = IBM_Plex_Serif({ subsets: ["latin"], weight: ["400", "700"], variable: "--font-ibm-plex-serif" });
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
-
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
-const inter = Inter({
-  variable: "--font-inter",
-  subsets: ["latin"],
-});
-
-const ibmPlexSerif = IBM_Plex_Serif({
-  variable: "--font-ibm-plex-serif",
-  weight:["400", "700"],
-  subsets: ["latin"],
-});
-
 export const metadata: Metadata = {
   title: "Horizon",
-  description: "Horizon is a modern banking platform for everyone",
-  icons: 
+  description: "Horizon is modern banking platform for everyone",
+  icons:
   {
-    icon: `/icons/logo.svg`
+    icon:`/icons/logo.svg`
   }
 };
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -47,3 +35,6 @@ export default function RootLayout({
     </html>
   );
 }
+
+
+
